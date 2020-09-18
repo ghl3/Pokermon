4ac16d213af69efc24c87b1925d2341ecc92cd12
George Lewis <ghl227@gmail.com>
2020-09-18 18:01:03 -0400

    Add training script

--- pokermon/training/train.py
+++ pokermon/training/train.py
@@ -1,8 +1,13 @@
+import argparse
 import logging
-from random import randint
+import sys
+from random import randint, shuffle
 from typing import List
 
+from tqdm import tqdm
+
 import pokermon.poker.rules as rules
+from pokermon.model import heads_up
 from pokermon.model.heads_up import HeadsUpModel
 from pokermon.poker import dealer
 from pokermon.poker.cards import FullDeal
@@ -18,8 +23,10 @@ def choose_starting_stacks():
 def train_heads_up(players: List[HeadsUpModel], num_hands_to_play: int):
     logger.debug("Beginning training with %s players", len(players))
 
-    for hand_index in range(num_hands_to_play):
+    for _ in tqdm(range(num_hands_to_play)):
         starting_stacks = choose_starting_stacks()
+
+        shuffle(players)
 
         deal: FullDeal = dealer.deal_cards(len(players))
 
@@ -35,3 +42,39 @@ def train_heads_up(players: List[HeadsUpModel], num_hands_to_play: int):
                 deal.board,
                 results,
             )
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Play a hand of poker.")
+
+    parser.add_argument(
+        "--num_hands",
+        help="Number of hands to play",
+        type=int,
+        default=100,
+    )
+
+    parser.add_argument(
+        "-log",
+        "--log",
+        help="Provide logging level. Example --log debug'",
+        type=str,
+        default="DEBUG",
+    )
+
+    args = parser.parse_args()
+
+    # Configure the logger
+    format = "[%(asctime)s] %(pathname)s:%(lineno)d %(levelname)s - %(message)s"
+    log_level = getattr(logging, args.log)
+    logging.basicConfig(level=log_level, format=format)
+
+    models = [heads_up.HeadsUpModel("Foo"), heads_up.HeadsUpModel("Bar")]
+
+    train_heads_up(models, args.num_hands)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
