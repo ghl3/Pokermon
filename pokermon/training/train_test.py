95d9f037976f15dd515439102a928cfa20fc54f1
George Lewis <ghl227@gmail.com>
2020-09-24 18:18:02 -0400

    Don't save during test

--- pokermon/training/train_test.py
+++ pokermon/training/train_test.py
@@ -4,4 +4,4 @@ from pokermon.training import train
 
 def test_heads_up():
     models = {"foo": heads_up.HeadsUpModel("Foo"), "bar": heads_up.HeadsUpModel("Bar")}
-    train.train_heads_up(models, 2, 5)
+    train.train_heads_up(models, 2)
