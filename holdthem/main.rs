c41bcf9d27714a5e817275dca774bd951cd81712
George Lewis <ghl227@gmail.com>
2020-10-24 18:21:46 -0400

    Make python eval function

--- holdthem/main.rs
+++ holdthem/main.rs
@@ -1,52 +1,14 @@
 mod simulate;
 
-//extern crate rs_poker;
 use crate::simulate::{simulate, Game};
 use rs_poker::core::{Card, Deck, Flattenable, Hand, Suit, Value};
 use rs_poker::holdem::MonteCarloGame;
-
-// This is a comment, and is ignored by the compiler
-// You can test this code by clicking the "Run" button over there ->
-// or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
-
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
-
-// This is the main function
 
 pub fn card_from_char(card_str: &str) -> Result<Card, String> {
     let value = Value::from_char(card_str.chars().nth(0).unwrap()).unwrap();
     let suit = Suit::from_char(card_str.chars().nth(1).unwrap()).unwrap();
     Ok(Card { value, suit })
 }
-//
-// pub fn new_with_hands_and_board(
-//     hands: Vec<Hand>,
-//     board: Vec<Card>,
-// ) -> Result<MonteCarloGame, String> {
-//     let mut d = Deck::default();
-//     for h in &hands {
-//         if h.len() != 2 {
-//             return Err(String::from("Hand passed in doesn't have 2 cards."));
-//         }
-//         for c in h.iter() {
-//             if !d.remove(*c) {
-//                 return Err(format!("Card {} was already removed from the deck.", c));
-//             }
-//         }
-//     }
-//     for c in &board {
-//         if !d.remove(*c) {
-//             return Err(format!("Card {} was already removed from the deck.", c));
-//         }
-//     }
-//     Ok(MonteCarloGame {
-//         deck: d.flatten(),
-//         board: board,
-//         hands: hands,
-//         current_offset: 52,
-//     })
-// }
 
 fn main() {
     let hands = vec![
@@ -59,27 +21,14 @@ fn main() {
         card_from_char("8h").unwrap(),
     ];
 
-    // let mut g = new_with_hands_and_board(hands, board).expect("Should be able to create a game.");
     let win_counts = simulate(
         Game {
             hole_cards: hands,
-            board: board,
+            board,
         },
         1_000_000,
     )
     .unwrap();
-
-    // [0, 0];
-    // for _ in 0..10_000 {
-    //     match g.simulate() {
-    //         Ok((winner_index, rank)) => {
-    //             wins[winner_index] += 1;
-    //             print!("{:?}\n", rank)
-    //         }
-    //         _ => {}
-    //     }
-    //     g.reset();
-    // }
 
     println!("Wins = {:?}", win_counts);
 }
--- holdthem/pyholdthem.rs
+++ holdthem/pyholdthem.rs
@@ -1,15 +1,98 @@
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use pyo3::types::{PyModule, PyString, PyTuple};
 use pyo3::wrap_pyfunction;
+use rs_poker::core::{Card, Hand, Rank, Rankable, Suit, Value};
+use std::error::Error;
 
-/// Formats the sum of two numbers as string.
+pub fn card_from_str(card_str: &str) -> Result<Card, String> {
+    if card_str.len() != 2 {
+        return Err(String::from("Error"));
+    }
+
+    let value = Value::from_char(card_str.chars().nth(0).unwrap()).unwrap();
+    let suit = Suit::from_char(card_str.chars().nth(1).unwrap()).unwrap();
+    Ok(Card { value, suit })
+}
+
+#[pyclass]
+struct HandRank {
+    #[pyo3(get)]
+    rank: i32,
+    #[pyo3(get)]
+    kicker: i32,
+}
+
+#[derive(Debug)]
+struct CustomError {
+    message: String,
+}
+
+impl std::convert::From<String> for CustomError {
+    fn from(err: String) -> CustomError {
+        CustomError { message: err }
+    }
+}
+
+impl std::convert::From<CustomError> for PyErr {
+    fn from(err: CustomError) -> PyErr {
+        PyValueError::new_err(err.message)
+    }
+}
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn evaluate_hand(hole_cards: String, board: Vec<String>) -> Result<HandRank, CustomError> {
+    let mut hand = Hand::new_from_str(&hole_cards)?;
+    for card in &board {
+        hand.push(card_from_str(card)?)
+    }
+
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+
+    Ok(match hand.rank() {
+        Rank::HighCard(x) => HandRank {
+            rank: 1,
+            kicker: x as i32,
+        },
+        Rank::OnePair(x) => HandRank {
+            rank: 2,
+            kicker: x as i32,
+        },
+        Rank::TwoPair(x) => HandRank {
+            rank: 3,
+            kicker: x as i32,
+        },
+        Rank::ThreeOfAKind(x) => HandRank {
+            rank: 4,
+            kicker: x as i32,
+        },
+        Rank::Straight(x) => HandRank {
+            rank: 5,
+            kicker: x as i32,
+        },
+        Rank::Flush(x) => HandRank {
+            rank: 6,
+            kicker: x as i32,
+        },
+        Rank::FullHouse(x) => HandRank {
+            rank: 7,
+            kicker: x as i32,
+        },
+        Rank::FourOfAKind(x) => HandRank {
+            rank: 8,
+            kicker: x as i32,
+        },
+        Rank::StraightFlush(x) => HandRank {
+            rank: 9,
+            kicker: x as i32,
+        },
+    })
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyholdthem(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(evaluate_hand, m)?)?;
     Ok(())
 }
