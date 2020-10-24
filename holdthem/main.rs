f6540705c662595332f5b2c0ae5bf072e4fcca88
George Lewis <ghl227@gmail.com>
2020-10-24 14:02:05 -0400

    Fix cargo building

--- holdthem/main.rs
+++ holdthem/main.rs
@@ -1,3 +1,6 @@
+//extern crate rs_poker;
+use rs_poker::core::Hand;
+use rs_poker::holdem::MonteCarloGame;
 
 
 // This is a comment, and is ignored by the compiler
@@ -9,8 +12,16 @@
 
 // This is the main function
 fn main() {
-    // Statements here are executed when the compiled binary is called
-
-    // Print text to the console
-    println!("Hello World!");
+    let hands = ["Adkh", "8c8s"]
+        .iter()
+        .map(|s| Hand::new_from_str(s).expect("Should be able to create a hand."))
+        .collect();
+    let mut g = MonteCarloGame::new_with_hands(hands).expect("Should be able to create a game.");
+    let mut wins: [u64; 2] = [0, 0];
+    for _ in 0..10_000 {
+        let r = g.simulate().expect("There should be one best rank.");
+        g.reset();
+        wins[r.0] += 1
+    }
+    println!("Wins = {:?}", wins);
 }
